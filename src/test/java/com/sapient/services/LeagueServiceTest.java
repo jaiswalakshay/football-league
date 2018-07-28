96fe98d20b5a6d56bdeb089c9bc2cb9a6310a2b1
akshay <jaiswal740@gmail.com>
2018-07-28 14:39:04 +0530

    test folder

---
+++ src/test/java/com/sapient/services/LeagueServiceTest.java
@@ -0,0 +1,24 @@
+package com.sapient.services;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LeagueServiceTest {
+
+    @Test
+    public void getLeagueDetail() {
+    }
+
+    @Test
+    public void getLeagueIdByName() {
+    }
+
+    @Test
+    public void getCountryIdByName() {
+    }
+
+    @Test
+    public void getStandings() {
+    }
+}
