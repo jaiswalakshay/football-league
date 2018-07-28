81a801ca6aee90dda9cc1f741202a405ad0c7985
akshay <jaiswal740@gmail.com>
2018-07-28 14:02:50 +0530

    added scripts

--- run-app.sh
+++ run-app.sh
@@ -1,3 +1,5 @@
+mkdir -p deployment/logs
+
+mv football-league-0.0.1-SNAPSHOT.jar deployment/football-league.jar
 cd deployment
-mv football-league-0.0.1-SNAPSHOT.jar football-league.jar
 nohup java -jar football-league.jar 2>&1 > ~/deployment/logs/output.log &
